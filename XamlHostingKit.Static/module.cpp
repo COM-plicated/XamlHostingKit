b384675aa45fcd1137d26c751defb201134ebeee
ahmed605 <ahmed.walid605@yahoo.com>
2026-08-21 21:58:37 +0300

    begin work on the static version

---
+++ XamlHostingKit.Static/module.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "winrt/base.h"
+
+bool __stdcall XHK_can_unload_now() noexcept;
+void* __stdcall XHK_get_activation_factory([[maybe_unused]] std::wstring_view const& name);
+
+extern "C" int32_t __stdcall XHK_CanUnloadNow() noexcept
+{
+    return XHK_can_unload_now() ? 0 : 1;
+}
+
+extern "C" int32_t __stdcall XHK_GetActivationFactory(void* classId, void** factory) noexcept try
+{
+    std::wstring_view const name { *reinterpret_cast<winrt::hstring*>(&classId) };
+    *factory = XHK_get_activation_factory(name);
+
+    if (*factory)
+    {
+        return 0;
+    }
+
+    return winrt::hresult_class_not_available(name).to_abi();
+}
+catch (...) { return winrt::to_hresult(); }
